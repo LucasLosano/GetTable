6a9745965651f1bb22f5ea89de78325876436c64
unknown <losano3@hotmail.com>
2021-05-13 19:00:41 -0300

    Get table

--- Program.cs
+++ Program.cs
@@ -1,15 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Interactions;
-using NUnit.Framework;
 
 namespace GetTableSelenium
 {
@@ -20,6 +10,7 @@ namespace GetTableSelenium
             var table = GetTable.Get("https://www.w3schools.com/html/html_tables.asp", "customers");
 
             Console.Clear();
+            Console.WriteLine();
             foreach (string header in table.headers)
             {
                 Console.Write(header + " ");
